93418e612a484f3bb2152f9ff966bb871c3c94b3
Your Name <your-email@example.com>
2026-04-13 17:45:44 +0530

    super keyword

--- inheritance/boxWeight.java
+++ inheritance/boxWeight.java
@@ -6,4 +6,10 @@ public class boxWeight extends box {
     public boxWeight(){
         this.wt = 5;
     }
+
+    public boxWeight(double l, double h, double w, double wt){
+        super(l , h, w); //call the parent class constructor
+        //used to initialize values present in parent class.
+        this.wt = wt;
+    }
 }
--- inheritance/mainClass.java
+++ inheritance/mainClass.java
@@ -10,5 +10,11 @@ public class mainClass {
     boxWeight box3 = new boxWeight();
     System.out.println(box3.h + " " + box3.wt); //boxWeight only defines wt, 
     // but because it extends box, it automatically gets l, h, w from the parent.
+
+    boxWeight box4 = new boxWeight(2, 5, 7, 9);
+    System.out.println(box4.l + " " + box4.wt);
+
+
+
 }
 }
